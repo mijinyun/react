c34058b855becf2d157cc63425886aed9b71dd61
mijinyun <yunmj1224@gmail.com>
2022-10-06 17:00:34 +0900

    블로그 완성

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
@@ -6,13 +5,14 @@ function App() {
   //페이지에서 자주 바뀌는것은 state로 하는게 좋음.
   //state는 등호로 변경하면 안된다!!
 
-  let post = '강남 우동 맛집';
   let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','파이썬 독학']);  
-  // let [글제목2, b] = useState('여자 코트 추천');  
-  // let [글제목3, c] = useState('성인 코트 추천');  
-  let [따봉,따봉변경] = useState([0,0,0]); //따봉변경 = state변경용 함수 (이걸 써야 html 재렌더링이 잘됨!)
+  let [따봉,따봉변경] = useState([0,0,0]); 
 
   let [modal,setModal] = useState(false);  //:스위치역할
+  let [title,setTitle] = useState(0);
+  let [입력값,입력값변경] = useState('');
+
+  let [날짜, 날짜변경] = useState(['1월 4일','3월 2일','2월 8일']);
 
   const titleChange = () => {
     let copy = [...글제목];
@@ -25,7 +25,6 @@ function App() {
       <div className="black-nav">
         <h4> ReactBlog </h4>
       </div>
-      {/* 여기는 숙제~! -해결! */}
       <button onClick={ () => {
         let rearray = [...글제목];
         rearray.sort();
@@ -38,39 +37,60 @@ function App() {
         // copy[0] = '여자 코트 추천';
         // 글제목변경(copy);
       }>글수정</button>
-
-      {/* <div className = 'list'>
-        <h4>{ 글제목[0]} <span onClick={ () => { 따봉변경(따봉+1) } }>👍</span> {따봉} </h4>
-        <p>9월 21일 발행</p>
-      </div>
-      <div className = 'list'>
-        <h4>{ 글제목[1] }</h4>
-        <p>9월 22일 발행</p>
-      </div>
-      <div className = 'list'>
-        <h4 onClick={() => { modal == true ? setModal(false) : setModal(true)}}>{ 글제목[2] }</h4>
-        <p>9월 23일 발행</p>
-      </div> */}
       
       {
         글제목.map(function(a , i){
           return (
             <div className = 'list'>
-              <h4 onClick={() => {setModal(!modal)}}>{ a }</h4>
-              <h4>{글제목[i]} <span onClick={() => {
+              <h4 key={i} onClick={() => {setModal(!modal); setTitle(i)}}>{ a }
+              <span onClick={(e) => {
+                e.stopPropagation();
                 let copy = [...따봉];
                 copy[i] += 1;
                 따봉변경(copy)}}
                 >👍</span> {따봉[i]}</h4>
-              <p>9월 22일 발행</p>
+              <p><span>{날짜[i]}</span>발행</p>
+              <button onClick={() => { 
+                let copy = [...글제목];
+                //copy 여기서 원하는 자료 삭제
+                copy.splice(i,1);
+                글제목변경(copy);
+              }}>삭제</button>
             </div>
           )
         })
       }
 
+      <input id="input" onChange={(e) => { 입력값변경(e.target.value);}}></input>
+      <button onClick={() => {
+
+        // 응용1. 입력값 없으면 발행 막기
+        if (document.getElementById('input').value.length == 0 ){
+          return false;
+        }
+
+        let copy = [입력값, ...글제목];
+        console.log(copy);
+        글제목변경(copy);
+
+        // 응용2. 글제목 추가되면 따봉도 같이 추가되게 하기
+        let good = [...따봉];
+        good.push(0);
+        따봉변경(good);
+
+        // 응용3. 현재 날짜 추가하기~
+        let today = [...날짜];
+        let date = new Date();
+        let uploadDate = date.getMonth() + '월 ' + date.getDay() + '일' 
+        today.unshift(uploadDate);
+        날짜변경(today);
+
+      }}>글발행</button>
+
       {
-        modal == true ? <Modal color={'skyblue'} 글제목={글제목} titleChange={titleChange}/> : null
+        modal == true ? <Modal color={'skyblue'} title={title} 글제목={글제목} titleChange={titleChange}/> : null
       }
+
 
     </div>
   );
@@ -79,12 +99,15 @@ function App() {
 function Modal(props) {
   return (
     <div className="modal" style={{background: props.color}}>
-        <h4>{props.글제목[0]}</h4>
+        <h4>{props.글제목[props.title]}</h4>
         <p>날짜</p>
         <p>상세내용</p>
         <button onClick={props.titleChange}>글수정</button>
     </div>
   )
 }
+
+
+
 
 export default App;
